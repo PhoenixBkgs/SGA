57fe1446874f37e800e1458741bcaae02f16c190
Eugene Kim <komastar2@gmail.com>
2017-10-16 23:44:22 +0900

    implement GameObject

--- WinAPI_Template/WinAPI_Template/GameObject.cpp
+++ WinAPI_Template/WinAPI_Template/GameObject.cpp
@@ -3,9 +3,31 @@
 
 
 GameObject::GameObject()
+    : m_uId(-1)
+    , m_szTagName("none")
+    , m_imgBody(NULL)
+    , m_dPos(UnitPos{ W_WIDTH * 0.5f, W_HEIGHT * 0.5f })
+    , m_dSpeed(UnitPos{ 0.0f, 0.0f })
+    , m_nSize(UnitSize{ 1, 1 })
+    , m_isVisible(true)
+    , m_isAlive(true)
+    , m_dAlpha(0.0f)
+    , m_dAngle(0.0f)
 {
 }
 
 GameObject::~GameObject()
+{
+}
+
+void GameObject::Update()
+{
+}
+
+void GameObject::Render()
+{
+}
+
+void GameObject::Move()
 {
 }
--- WinAPI_Template/WinAPI_Template/GameObject.h
+++ WinAPI_Template/WinAPI_Template/GameObject.h
@@ -1,12 +1,64 @@
 #pragma once
+
+//  Mutual Refernce
+class ImageObject;
+
 class GameObject
 {
 //  VARS
-public:
+private:
+    int             m_uId;
+    string          m_szTagName;
+    RECT            m_rtBody;
+    ImageObject*    m_imgBody;
+    RECT            m_rtHitBox;
+    
+    UnitPos         m_dPos;
+    UnitSpeed       m_dSpeed;
+    UnitSize        m_nSize;
+
+    bool            m_isVisible;
+    bool            m_isAlive;
+    double          m_dAlpha;
+    double          m_dAngle;
+
 //  FUNCS
 public:
     GameObject();
     ~GameObject();
+#pragma region GET
+    //  GET
+    int         GetUid() { return m_uId; }
+    string      GetTagName() { return m_szTagName; }
+    RECT        GetBodyRect() { return m_rtBody; }
+    RECT        GetHitBoxRect() { return m_rtHitBox; }
+    UnitPos     GetPos() { return m_dPos; }
+    UnitSize    GetSize() { return m_nSize; }
+    UnitSpeed   GetSpeed() { return m_dSpeed; }
+    bool        IsVisible() { return m_isVisible; }
+    bool        IsAlive() { return m_isAlive; }
+#pragma endregion
+
+#pragma region SET
+    //  SET
+    void SetUid(int Id) { m_uId = Id; }
+    void SetTagName(string TagName) { m_szTagName = TagName; }
+    void SetImgBody(ImageObject* Image) { m_imgBody = Image; }
+    void SetSize(UnitSize Size) { m_nSize = Size; }
+    void SetSpeed(UnitSpeed Speed) { m_dSpeed = Speed; }
+    void SetPos(UnitPos Pos) { m_dPos; }
+    void SetVisible() { m_isVisible = true; }
+    void SetInvisible() { m_isVisible = false; }
+    void SetAlive() { m_isAlive = true; }
+    void SetDead() { m_isAlive = false; }
+#pragma endregion
+
+    //  Update / Render
+    virtual void Update();
+    virtual void Render();
+
+    //  Object move
+    virtual void Move();
 
 };
 
