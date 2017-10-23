a8589f0a91af510b5cd66376a8b671b981905c46
Eugene Kim <komastar.dev@gmail.com>
2017-10-23 11:41:44 +0900

    write batch code (for template codes managing)

---
+++ lib-copy.bat
@@ -0,0 +1,8 @@
+copy .\WinAPI_Template\WinAPI_Template\*Object.cpp .\KM_Library\Obejcts\
+copy .\WinAPI_Template\WinAPI_Template\*Object.h .\KM_Library\Objects\
+
+copy .\WinAPI_Template\WinAPI_Template\*Manager.cpp .\KM_Library\Managers\
+copy .\WinAPI_Template\WinAPI_Template\*Manager.h .\KM_Library\Managers\
+
+copy .\WinAPI_Template\WinAPI_Template\*Scene.cpp .\KM_Library\Scenes\
+copy .\WinAPI_Template\WinAPI_Template\*Scene.h .\KM_Library\Scenes\
